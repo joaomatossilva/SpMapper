58a25ba2ededa1fcdcb2ade8e4f27f8ebffa16ee
Joao Silva <kappy@acydburne.com.pt>
2012-02-21 14:06:34 +0000

    Added SpMapper-Client Project for using with Sharepoint client obejct model

--- SpMapper-Client/Properties/AssemblyInfo.cs
+++ SpMapper-Client/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SpMapper")]
-[assembly: AssemblyDescription("A simple object mapper to sharepoint lists")]
+[assembly: AssemblyTitle("SpMapper-Client")]
+[assembly: AssemblyDescription("A simple object mapper to sharepoint lists, using the client object model")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f1b6ad60-a3b7-47ce-9b53-47d0a2726d44")]
+[assembly: Guid("80a1aab3-2f51-47d5-87c6-acd8fcfeb4eb")]
