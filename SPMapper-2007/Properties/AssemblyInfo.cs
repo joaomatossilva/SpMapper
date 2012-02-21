7eeddf4a8224d76231d713bb7e3b8db791a4b1ee
Joao Silva <kappy@acydburne.com.pt>
2012-02-21 14:25:24 +0000

    Added Sharepoint 2007 project

---
+++ SPMapper-2007/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SPMapper-2007")]
+[assembly: AssemblyDescription("A simple object mapper to sharepoint lists, for Sharepoint 2007")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("595164bd-796a-4fc9-abe5-26b7c0eb9f8d")]
