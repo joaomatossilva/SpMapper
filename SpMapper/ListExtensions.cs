99b700b8873788c0780f3db1ace8fd005f4cf90d
Joao Silva <kappy@acydburne.com.pt>
2012-02-21 14:08:30 +0000

    added support for sharepoint 2010 client object model. the type and list are now properly analised on first query to get the matched properties and types

--- SpMapper/ListExtensions.cs
+++ SpMapper/ListExtensions.cs
@@ -4,12 +4,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+#if SPCLIENT2010
+using Microsoft.SharePoint.Client;
+#else
 using Microsoft.SharePoint;
+#endif
 
 namespace SpMapper {
 
 	public static class ListExtensions {
 
+		class PropertyMap {
+			public PropertyInfo Property { get; set; }
+			public Type PropertyType { get; set; }
+			public bool IsNullableType { get; set; }
+			public string MapToFieldName { get; set; }
+		}
+
+#if SPCLIENT2010
+		public static IEnumerable<T> Query<T>(this List list, string camlQuery) where T : new() {
+			var query = new CamlQuery { ViewXml = camlQuery };
+			return Query<T>(list, query);
+		}
+
+		public static IEnumerable<T> Query<T>(this List list, CamlQuery query) where T : new() {
+			var items = list.GetItems(query);
+			var map = BuildMap(typeof(T), list.Fields.Select( field => field.InternalName));
+			foreach (ListItem item in items) {
+				yield return BuildObject<T>(map, fieldName => item[fieldName]);
+			}
+		}
+#else
 		public static IEnumerable<T> Query<T>(this SPList list, string camlQuery) where T : new() {
 			var query = new SPQuery { Query = camlQuery };
 			return Query<T>(list, query);
@@ -17,31 +42,44 @@ namespace SpMapper {
 
 		public static IEnumerable<T> Query<T>(this SPList list, SPQuery query) where T : new() {
 			var items = list.GetItems(query);
-			var settablePropertiesOfType = DiscoverSettableProperties(typeof(T));
+			var fieldNames = new List<string>();
+			foreach (SPField field in list.Fields) {
+				fieldNames.Add(field.InternalName);
+			}
+			var map = BuildMap(typeof(T), fieldNames);
 			foreach (SPListItem item in items) {
-				yield return Buildobject<T>(settablePropertiesOfType, item);
+				yield return BuildObject<T>(map, fieldName => item[fieldName]);
 			}
 		}
+#endif
 
-		private static PropertyInfo[] DiscoverSettableProperties(Type type) {
+		private static IEnumerable<PropertyMap> BuildMap(Type type, IEnumerable<string> fieldNames) {
+			var typeMap = new List<PropertyMap>();
 			var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.SetProperty);
-			return properties;
+			foreach (var property in properties) {
+				var fieldName = fieldNames.FirstOrDefault(field => field.Equals(property.Name, StringComparison.InvariantCultureIgnoreCase));
+				if (!string.IsNullOrEmpty(fieldName)) {
+					var propertyMap = new PropertyMap { Property = property, MapToFieldName = fieldName };
+					var propertyType = property.PropertyType;
+					if (propertyType.IsGenericType && propertyType.GetGenericTypeDefinition() == typeof(Nullable<>)) {
+						propertyMap.IsNullableType = true;
+						propertyMap.PropertyType = Nullable.GetUnderlyingType(propertyType);
+					} else {
+						propertyMap.PropertyType = propertyType;
+					}
+					typeMap.Add(propertyMap);
+				}
+			}
+			return typeMap;
 		}
 
-		private static T Buildobject<T>(PropertyInfo[] settableProperties, SPListItem item) where T : new() {
-			T obj = new T();
-			foreach (var settableProperty in settableProperties ) {
-				if (item.Fields.ContainsField(settableProperty.Name)) {
-					var propertyType = settableProperty.PropertyType;
-					object settablePropertyValue;
-					if (propertyType.IsGenericType && propertyType.GetGenericTypeDefinition() == typeof(Nullable<>)) {
-						Type underlyingType = Nullable.GetUnderlyingType(propertyType);
-						settablePropertyValue = Convert.ChangeType(item[settableProperty.Name], underlyingType);
-					} else {
-						settablePropertyValue = Convert.ChangeType(item[settableProperty.Name], propertyType);						
-					}
-					settableProperty.SetValue(obj, settablePropertyValue, null);
-				}
+
+
+		private static T BuildObject<T>(IEnumerable<PropertyMap> typeMap, Func<string, object> getValue) where T : new() {
+			var obj = new T();
+			foreach (var propertyMap in typeMap) {
+				object settablePropertyValue = Convert.ChangeType(getValue(propertyMap.MapToFieldName), propertyMap.PropertyType);
+				propertyMap.Property.SetValue(obj, settablePropertyValue, null);
 			}
 			return obj;
 		}
