24ab238ae62a20afb08dca4eb993ace7573b9140
Joao Silva <kappy@acydburne.com.pt>
2012-02-21 14:26:10 +0000

    updated library metadata

--- SpMapper/Properties/AssemblyInfo.cs
+++ SpMapper/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SpMapper")]
-[assembly: AssemblyDescription("A simple object mapper to sharepoint lists")]
+[assembly: AssemblyDescription("A simple object mapper to sharepoint lists, using the object model")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f1b6ad60-a3b7-47ce-9b53-47d0a2726d44")]
